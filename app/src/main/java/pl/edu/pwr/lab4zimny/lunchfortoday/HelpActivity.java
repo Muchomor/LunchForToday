5d64f66f056b5e517bbf9a438f929bf61e560a82
Jakub Zimny <jakubzimnyy@gmail.com>
2017-06-06 20:17:40 +0200

    first commit

---
+++ app/src/main/java/pl/edu/pwr/lab4zimny/lunchfortoday/HelpActivity.java
@@ -0,0 +1,17 @@
+package pl.edu.pwr.lab4zimny.lunchfortoday;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by KubaLaptop on 06.06.2017.
+ */
+
+public class HelpActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+    }
+}
